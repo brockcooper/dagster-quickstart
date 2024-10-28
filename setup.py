61885fcdf2fddb2326f842e3478168ec8d2a8936
Brock Cooper <brockcooper@gmail.com>
2024-10-28 21:54:28 +0000

    add packages to setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="dagster_quickstart",
-    packages=find_packages(exclude=["dagster_quickstart_tests"]),
+    packages=find_packages(include=["dagster_quickstart", "dagster_se_interview", "dagster_quickstart.*", "dagster_se_interview.*"]),
     install_requires=[
         "dagster",
         "dagster-cloud",
