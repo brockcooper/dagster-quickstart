7a6c71146ae8772e66be60bcfa1311ffa18510c4
Brock Cooper <brockcooper@gmail.com>
2024-10-28 21:59:58 +0000

    add stats function

--- dagster_se_interview/assets.py
+++ dagster_se_interview/assets.py
@@ -19,6 +19,25 @@ def change_model(country_stats: DataFrame) -> Regression:
 
 @asset
 def continent_stats(country_stats: DataFrame, change_model: Regression) -> DataFrame:
-    """ Summary of continent populations and change """
-    # TODO 
-    return 
+    """ Summary of continent populations and predicted change """
+    # Calculate basic stats
+    continent_summary = country_stats.groupby("continent").agg(
+        avg_pop_2022=("pop_2022", "mean"),
+        avg_pop_2023=("pop_2023", "mean"),
+        avg_pop_change=("pop_change", "mean"),
+        total_pop_2022=("pop_2022", "sum"),
+        total_pop_2023=("pop_2023", "sum")
+    ).reset_index()
+    
+    # Prepare dummy variables for prediction
+    dummies = get_dummies(continent_summary[["continent"]])
+    
+    # Predict population change using the model
+    continent_summary["predicted_pop_change"] = change_model.predict(dummies)
+    
+    # Calculate the expected 2024 population based on predicted change
+    continent_summary["predicted_pop_2024"] = (
+        continent_summary["avg_pop_2023"] * (1 + continent_summary["predicted_pop_change"] / 100)
+    )
+    
+    return continent_summary
